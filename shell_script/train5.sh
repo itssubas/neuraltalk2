1fccab767410f7fd777180d8bb6215e2794a025b
Sujit Maharjan <sujit@fusemachines.com>
2017-10-16 04:59:39 +0000

    shell_script for different training models

---
+++ shell_script/train5.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=2
+python train.py --id att2in --caption_model att2in --input_json data/output/cocotalk.json --input_fc_dir data/output/cocotalk_fc --input_att_dir data/cocotalk_att --input_label_h5 data/output/cocotalk_label.h5 --batch_size 100 --learning_rate 5e-4 --learning_rate_decay_start 0 --scheduled_sampling_start 0 --checkpoint_path log_att2in --save_checkpoint_every 6000 --val_images_use 5000 --max_epochs 25
+
